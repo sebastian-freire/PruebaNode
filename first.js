146f864d209fbbda2e46d69136e34973a82c931e
sebastian-freire <SEBASTIAN.FREIRE@correo.ucu.edu.uy>
2025-03-17 20:32:28 -0300

    async api

--- first.js
+++ first.js
@@ -1,4 +1,12 @@
 async function main() {
   console.log("Hola mundo");
+
+  const req = await fetch(
+    "https://openlibrary.org/search.json?q=the+lord+of+the+rings"
+  );
+  if (req.ok) {
+    const data = await req.json();
+    console.log(data);
+  }
 }
 main();
